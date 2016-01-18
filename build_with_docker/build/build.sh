d100f7a7d3256d746b842547cda90aee963481d5
reiki4040 <reiki4040@gmail.com>
2016-01-18 23:34:49 +0900

    added guide message.

--- build_with_docker/build/build.sh
+++ build_with_docker/build/build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # build docker image for go build
+echo "build golang base and toolchain..."
 docker build -t golangbuildbase:latest -f Dockerfile.base .
 
-docker build --no-cache -t hello-go:latest -f Dockerfile.build .
+echo -e "\nclone target project..."
+docker build -t hello-go:latest --no-cache -f Dockerfile.build .
 
 # for binary
 OUTPUT_DIR="./bin"
 mkdir -p $OUTPUT_DIR
 
+echo -e "\nmake binary..."
 # build
 docker run --volume="$(pwd)/$OUTPUT_DIR:/go/src/github.com/reiki4040/go-memo/build_with_docker/bin" -w /go/src/github.com/reiki4040/go-memo/build_with_docker hello-go /bin/bash go_build.sh
 
