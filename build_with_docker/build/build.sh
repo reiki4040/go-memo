b56e30a2d8a137891c2092d71f158456532195a7
reiki4040 <reiki4040@gmail.com>
2016-04-12 22:29:42 +0900

    improve build script.
    - error check
    - glide install

--- build_with_docker/build/build.sh
+++ build_with_docker/build/build.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-# build docker image for go build
-echo "build golang base and toolchain..."
-docker build -t golangbuildbase:latest -f Dockerfile.base .
+# test docker command
+rs=$(docker ps)
+if [ $? -ne 0 ]; then
+  echo "your docker machine is running?"
+  exit 1
+fi
 
-echo -e "\nclone target project..."
+# build docker image for go build
+echo "build golang base and install toolchain to docker image..."
+docker build -t golangbuildbase:latest -f Dockerfile.base .
+if [ $? -eq 1 ]; then
+  echo "failed build base image."
+  exit 1
+fi
+
+echo -e "\nclone target project into docker image..."
 docker build -t hello-go:latest --no-cache -f Dockerfile.build .
+if [ $? -eq 1 ]; then
+  echo "failed build hello-go image."
+  exit 1
+fi
 
 # for binary
 OUTPUT_DIR="./bin"
 mkdir -p $OUTPUT_DIR
 
-echo -e "\nmake binary..."
+echo -e "\nstart make binary..."
 # build
 docker run --volume="$(pwd)/$OUTPUT_DIR:/go/src/github.com/reiki4040/go-memo/build_with_docker/bin" -w /go/src/github.com/reiki4040/go-memo/build_with_docker hello-go /bin/bash go_build.sh
-
+if [ $? -eq 1 ]; then
+  echo "failed make binary."
+  exit 1
+else
+  echo "successed make binary."
+fi
--- build_with_docker/go_build.sh
+++ build_with_docker/go_build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-glide up
+glide install
 
 VERSION=0.1.0
 HASH=$(git rev-parse --verify HEAD)
 GOVERSION=$(go version)
 
-gox -output="bin/hello-go_{{.OS}}_{{.Arch}}" -os="linux" -os="darwin" -arch="amd64" -ldflags "-X main.version=$VERSION -X main.hash=$HASH -X \"main.goversion=$GOVERSION\""
+gox -output="bin/hello-go_{{.OS}}_{{.Arch}}" \
+	-os="linux" -os="darwin" -arch="amd64" \
+	-ldflags "-X main.version=$VERSION -X main.hash=$HASH -X \"main.goversion=$GOVERSION\""
